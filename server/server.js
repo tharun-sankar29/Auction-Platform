199c388813d18f4ffda109ba20fd074c9ae51037
Tharun Sankar <tharunsankarmohanraj@gmail.com>
2025-03-24 22:17:10 +0530

    new server init

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { error } = require('console');
 
 const app = express();
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Correct parameter order (req, res)
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './home.html'));
+// Serve static files from multiple folders
+app.use('/css', express.static(path.join(__dirname, '../css')));  // CSS folder
+app.use('/js', express.static(path.join(__dirname, '../js')));    // JS folder
+app.use('/', express.static(path.join(__dirname, '../')));        // HTML files in the root
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../home.html'));  // Serve home.html
 });
+
+app.post('/login', (req, res)) {
+    const {username, password} = req.body;
+    
+}
 
 app.listen(3000, () => {
     console.log('Listening on port 3000...');
