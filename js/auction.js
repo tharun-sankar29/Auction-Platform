278215a75088a0ebf10754b83e056ea4e4a5cd36
Tharun Sankar <tharunsankarmohanraj@gmail.com>
2025-03-30 18:21:11 +0530

    error in app

--- js/auction.js
+++ js/auction.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
 
-const app = express();
-app.use(express.urlencoded({ extended: true}));
 const url = 'mongodb://127.0.0.1:27017/sample';
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
