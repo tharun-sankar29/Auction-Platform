09fbf824e9b656b76abfa97ff8c1563edf8e3869
Tharun Sankar <tharunsankarmohanraj@gmail.com>
2025-03-30 22:53:57 +0530

    rmoved redundant routing

--- server/server.js
+++ server/server.js
@@ -24,59 +24,7 @@ app.use(session({
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-//authentication for session
-// app.post('/auth/login', async (req, res) => {
-//     const { email, password } = req.body;
-//     try {
-//       const user = await User.findOne({ email });
-//       if (user && user.password === password) {
-//         req.session.userId = user._id;
-//         res.redirect('/profile');
-//       } else {
-//         res.status(401).send('Invalid credentials');
-//       }
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).send('Server error');
-//     }
-//   });
 
-
-app.get('../profile.ejs.html', async (req, res) => {
-    if (!req.session.userId) {
-      return res.status(401).json('No such user');
-    }
-    try {
-      const userData = await User.findById(req.session.userId);
-      const soldData = await Auction.find({ seller_id: req.session.userId });
-      const activeAuctions = await Auction.find({ seller_id: req.session.userId, status: { $ne: "Dead" } });
-      const deadData = await Dead.find({ seller_id: req.session.userId });
-
-      const computeMaxBid = (auction) => {
-        let maxBid = 0;
-        auction.bids.forEach(bid => {
-          if (bid.amount > maxBid) {
-            maxBid = bid.amount;
-          }
-        });
-        return maxBid;
-      };
-  
-      soldData.forEach(auction => auction.maxamount = computeMaxBid(auction));
-      activeAuctions.forEach(auction => auction.maxamount = computeMaxBid(auction));
-      deadData.forEach(dead => dead.maxamount = computeMaxBid(dead));
-      
-      res.render('../profile.ejs.html', { 
-        user: userData, 
-        soldProduct: soldData, 
-        auction: activeAuctions, 
-        Deadauction: deadData,
-      });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server error');
-    }
-  });
 
 app.get('../feedback.ejs.html', async (req,res) => {
     try {
